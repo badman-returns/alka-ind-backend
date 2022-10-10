a74dd504d82bb38e5b5d11922753a3ef9f6f9136
tsgoswami <trishnangshugoswami@yahoo.in>
2022-10-10 22:15:31 +0530

    about interface

---
+++ src/interfaces/about.model.ts
@@ -0,0 +1,14 @@
+interface About {
+    id?: number;
+    title: string;
+    content: string;
+    fileId?: string;
+    fileURL?: string;
+    createdBy: number;
+    updatedBy?: number;
+    updatedOn?: string; 
+}
+
+export {
+    About
+}
