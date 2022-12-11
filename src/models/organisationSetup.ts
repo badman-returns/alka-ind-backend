4f238883fce279435b2ac2af65df830315bfec0f
tsgoswami <trishnangshugoswami@yahoo.in>
2022-12-11 10:50:36 +0530

    file upload logic seperated

--- src/models/organisationSetup.ts
+++ src/models/organisationSetup.ts
@@ -9,20 +9,28 @@ export class OrganisationDB {
 
     public static updateOrganisationInfo(name: string, phone: string, email: string, address: string, updatedBy:number, fileId?: string, fileURL?:string): Promise<any> {
         return new Promise((resolve, reject) => {
-            db.query(`UPDATE ${Tables.ORGANISATION} SET name=?, phone=?, email=?, address=?, fileId=?, fileURL=?, updatedBy=? WHERE id=1`, [name, phone, email, address, fileId, fileURL, updatedBy], (err) => {
-                if(err){
-                    return reject(err);
-                }
-                return resolve(null);
-            })
+            if(fileId && fileURL){
+                db.query(`UPDATE ${Tables.ORGANISATION} SET name=?, phone=?, email=?, address=?, fileId=?, fileURL=?, updatedBy=? WHERE id=1`, [name, phone, email, address, fileId, fileURL, updatedBy], (err) => {
+                    if(err){
+                        return reject(err);
+                    }
+                    return resolve(null);
+                })
+            } else {
+                db.query(`UPDATE ${Tables.ORGANISATION} SET name=?, phone=?, email=?, address=?, updatedBy=? WHERE id=1`, [name, phone, email, address, updatedBy], (err) => {
+                    if(err){
+                        return reject(err);
+                    }
+                    return resolve(null);
+                })
+            }
+           
         })
     }
 
     public static getOrganisationInfo():Promise<OrganisationInfo>{
         return new Promise((resolve, reject) => {
-            db.query(`SELECT ${Tables.ORGANISATION}.id, ${Tables.ORGANISATION}.name, ${Tables.ORGANISATION}.email, ${Tables.ORGANISATION}.address, ${Tables.ORGANISATION}.fileURL, ${Tables.USER}.name as createdBy, ${Tables.USER}.name as updatedBy 
-                      FROM ${Tables.ORGANISATION}
-                      INNER JOIN ${Tables.USER} ON ${Tables.USER}.id=${Tables.ORGANISATION}.updatedBy
+            db.query(`SELECT * FROM ${Tables.ORGANISATION}
                       `, (err, res) => {
                 if(err){
                     return reject(err);
