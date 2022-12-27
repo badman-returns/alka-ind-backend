4fc19948dd86f6f8072a3f2d6aa93c243567e80d
tsgoswami <trishnangshugoswami@yahoo.in>
2022-12-28 01:59:52 +0530

    file upload condition check

--- src/models/partner.ts
+++ src/models/partner.ts
@@ -3,63 +3,72 @@ import { Tables } from '../configs/table.config';
 import db from './db';
 
 export class PartnerDB {
-    constructor(){
+    constructor() {
 
     }
 
-    public static getAllPartner():Promise<Array<Partner>> {
+    public static getAllPartner(): Promise<Array<Partner>> {
         return new Promise((resolve, reject) => {
             db.query(`SELECT * FROM ${Tables.PARTNER}`, (err, res) => {
-                if(err){
+                if (err) {
                     return reject(err);
                 }
-                if(res.length) {
-                    return resolve(res.map((result: any) =>  Object.assign({}, result)));
+                if (res.length) {
+                    return resolve(res.map((result: any) => Object.assign({}, result)));
                 }
                 return resolve(null);
             });
         });
     };
 
-    public static getPartnerById(id: number): Promise<Partner>{
+    public static getPartnerById(id: number): Promise<Partner> {
         return new Promise((resolve, reject) => {
             db.query(`SELECT * FROM ${Tables.PARTNER} WHERE id=${id}`, (err, res) => {
-                if(err){
+                if (err) {
                     return reject(err);
                 }
-                if(res.length) {
+                if (res.length) {
                     return resolve(res.map((result: any) => Object.assign({}, result))[0]);
                 }
             })
         })
     }
 
-    public static insertPartner(name: string, fileId: string, fileURL: string, createdBy: number, testimony?:string): Promise<string>{
+    public static insertPartner(name: string, createdBy: number, testimony?: string, fileId?: string, fileURL?: string,): Promise<string> {
         return new Promise((resolve, reject) => {
-            db.query(`INSERT INTO ${Tables.PARTNER} (name, testimony, fileId, fileURL, createdBy) VALUES ('${name}', '${testimony}', '${fileId}', '${fileURL}', '${createdBy}')`, (err, res) => {
-                if(err){
+            db.query(`INSERT INTO ${Tables.PARTNER} (name, testimony, fileId, fileURL, createdBy) VALUES ('${name}', "${testimony}", '${fileId}', '${fileURL}', '${createdBy}')`, (err, res) => {
+                if (err) {
                     return reject(err);
-                }    
+                }
                 return resolve(res.insertId);
             });
         });
     };
 
-    public static updatePartnerById(id: number, name:string, fileId: string, fileURL: string, testimony?: string): Promise<any>{
+    public static updatePartnerById(id: number, name: string, testimony?: string, fileId?: string, fileURL?: string): Promise<any> {
         return new Promise((resolve, reject) => {
-            db.query(`UPDATE ${Tables.PARTNER} SET name=?, testimony=?, fileId=?, fileURL=? WHERE ${Tables.PARTNER}.id=${id}`, [name, testimony,fileId, fileURL], (err, res) => {
-                if(err){
-                    return reject(err);
-                }
-                return resolve(null);
-            });
+            if (fileId && fileURL) {
+                db.query(`UPDATE ${Tables.PARTNER} SET name=?, testimony=?, fileId=?, fileURL=? WHERE ${Tables.PARTNER}.id=${id}`, [name, testimony, fileId, fileURL], (err, res) => {
+                    if (err) {
+                        return reject(err);
+                    }
+                    return resolve(null);
+                });
+            } else {
+                db.query(`UPDATE ${Tables.PARTNER} SET name=?, testimony=? WHERE ${Tables.PARTNER}.id=${id}`, [name, testimony], (err, res) => {
+                    if (err) {
+                        return reject(err);
+                    }
+                    return resolve(null);
+                });
+            }
         });
     };
 
-    public static deletePartnerById(id: number): Promise<any>{
+    public static deletePartnerById(id: number): Promise<any> {
         return new Promise((resolve, reject) => {
             db.query(`DELETE FROM ${Tables.PARTNER} WHERE ${Tables.PARTNER}.id=${id}`, (err, res) => {
-                if(err){
+                if (err) {
                     return reject(err);
                 }
                 return resolve(null);
